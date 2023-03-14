5aba61b80c41b4de3a78f64121eba307beb1f28f
Romain <romain.jayez@gmail.com>
2023-03-14 21:48:41 +0100

    feat: mylou appear animation

--- src/GameButton.tsx
+++ src/GameButton.tsx
@@ -2,11 +2,9 @@ import styles from "./GameButton.module.css";
 
 export function GameButton(props: { active: boolean, onClick: () => void }) {
 
-
-
-    return (<button class={styles.gameButton} classList={{buttonActive: props.active}}
+    return (<button class={styles.gameButton}  classList={{buttonActive: props.active}}
                     onClick={props.onClick}>
-        <div class={styles.mylou}/>
+        <div class={styles.mylou} classList={{mylouActive : props.active}} />
         <div class={styles.nuage}/>
     </button>);
 }
